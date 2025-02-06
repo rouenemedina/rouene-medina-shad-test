9497044dd8b8d1e33447b3796bcd73d132f02101
rouenemedina <rouenemedina@gmail.com>
2025-02-06 06:12:35 -0700

    installed accordion ui component, implemented the accordion component for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Accordion, AccordionContent, AccordionTrigger, AccordionItem } from "./components/ui/accordion";
 
 function App() {
   return (
     <main>
       <h1>Hello World</h1>
       <Button>Hello</Button>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </main>
   );
 }
